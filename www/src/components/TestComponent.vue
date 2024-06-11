e1e7729c07cd32d5c24adc34c011a5d9b801a6fc
tokitouq <suneeth.since2005@gmail.com>
2024-06-11 20:18:17 +0530

    style: add reset.scss

--- www/src/components/TestComponent.vue
+++ www/src/components/TestComponent.vue
@@ -1,16 +1,9 @@
-<script setup lang="ts">
-import { Heart } from "lucide-vue-next"
-</script>
+<script setup lang="ts"></script>
 
 <template>
     <span class="test">Moonlit Grace</span>
-    <Heart />
 </template>
 
 <style lang="scss">
 @import "@/assets/scss/_variables.scss";
-
-.test {
-    color: $dark-color;
-}
 </style>
