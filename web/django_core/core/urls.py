ebf36824f9b9d69f0214dd1b9bfd3aebbb45e89e
tokitouq <suneeth.since2005@gmail.com>
2024-07-07 03:51:19 +0530

    feat: add base templates

--- web/django_core/core/urls.py
+++ web/django_core/core/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.shortcuts import render
 from django.contrib import admin
 from django.urls import path
 
+# demo view
+# TODO: remove this later
+def index(request):
+    return render(request, "index.html")
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", index)
 ]
